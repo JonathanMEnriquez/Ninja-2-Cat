cefe60c8362c352cab79567277c60c379340f547
user <user@users-MacBook-Pro.local>
2017-11-22 11:40:46 -0800

    initial commit

---
+++ code.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+
+
+    $("#wrapper img").click(function() {
+
+        var temp = $(this).attr("data-alt-src");
+        
+        $(this).attr("data-alt-src", $(this).attr("src"))
+        $(this).attr("src", temp);
+
+
+});
+
+});
+
